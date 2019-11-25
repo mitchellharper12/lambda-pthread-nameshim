ba176993f4cf18132353d43e39313be0c477871e
Mitchell Harper <mitchwharper@gmail.com>
2019-11-24 18:30:29 -0800

    Improve test to check thread name delta

--- test_shim.c
+++ test_shim.c
@@ -3,15 +3,24 @@
 #include <stdlib.h>
 
 int main() {
-	pthread_t tid = pthread_self();
-	char* buffer_2 = "name";
+	char buffer[20];
 	int iret;
-	iret = pthread_setname_np(tid, buffer_2);
+	pthread_t tid = pthread_self();
+	iret = pthread_getname_np(tid, buffer, 20);
 	if (iret != 0) {
 		printf("getname returned %d\n", iret);
 		exit(iret);
 	}
-	char buffer[20];
+	printf("Getname buffer: %s\n", buffer);
+	printf("Thread ID: %u\n", tid);
+
+	char* buffer_2 = "name";
+	iret = pthread_setname_np(tid, buffer_2);
+	if (iret != 0) {
+		printf("setname returned %d\n", iret);
+		exit(iret);
+	}
+
 	iret = pthread_getname_np(tid, buffer, 20);
 	if (iret != 0) {
 		printf("getname returned %d\n", iret);
